e2cf5a81d2e4495fa43438e5591a8fd5d3bfec06
EstebanOG <juanesteban199911@gmail.com>
2021-02-06 21:51:37 -0500

    Se genera matriz de adyacencia

---
+++ Kruskal/src/kruskal/Vista/PanelMatriz.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kruskal.Vista;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PanelMatriz extends JPanel {
+
+    int x = 0, y = 45, numPuntos = 0;
+    JLabel lblTituloMatriz, lblTituloMatriz2;
+    JTextField[] jTextField;
+    String[][] matriz;
+
+    public PanelMatriz() {
+        setLayout(null);
+        setPreferredSize(new Dimension(800,520));
+        setVisible(true);
+        iniciarComponentes();
+    }
+
+    private void iniciarComponentes() {
+        lblTituloMatriz = new JLabel();
+        lblTituloMatriz.setBounds(0, 0, 400, 30);
+        lblTituloMatriz.setText("<html>99 = Costo de una arista para vertices no adjuntos<br>X indica que la matriz es simétrica</html>");
+        add(lblTituloMatriz);
+    }
+
+    public void generarMatriz(int numPuntos) {
+        this.numPuntos = numPuntos;
+        
+        jTextField = new JTextField[(numPuntos + 2) * (numPuntos * 2)];
+        matriz = new String[numPuntos + 1][numPuntos + 1];
+        for (int i = 0; i <= numPuntos; i++) {
+            for (int j = 0; j <= numPuntos; j++) {
+                if (i == 0) {
+                    matriz[i][j] = j + "";
+                } else if (j == 0) {
+                    matriz[i][j] = i + "";
+                } else if (i == j) {
+                    matriz[i][j] = 0 + "";
+                } else if (j > i) {
+                    matriz[i][j] = 99 + "";
+                } else {
+                    matriz[i][j] = "X";
+                }
+            }
+        }
+        for (int i = 0; i <= numPuntos; i++) {
+            for (int j = 0; j <= numPuntos; j++) {
+                System.out.print(matriz[i][j] + "-");
+            }
+            System.out.println("");
+        }
+        generarComponentes();
+    }
+
+    private void generarComponentes() {
+        lblTituloMatriz2 = new JLabel();
+        lblTituloMatriz2.setBounds(60, 40, 400, 30);
+        lblTituloMatriz2.setText("Matriz de adyacencia");
+        lblTituloMatriz2.setFont(new Font("Arial", Font.PLAIN, 20));
+        add(lblTituloMatriz2);
+        int k = 0;
+        for (int i = 0; i <= numPuntos; i++) {
+            for (int j = 0; j <= numPuntos; j++) {
+                crearJTextFields(i, j, k);
+                k++;
+            }
+        }
+    }
+
+    private void crearJTextFields(int i, int j, int k) {
+        jTextField[k] = new JTextField(matriz[i][j]);
+        if(k!=0)
+            jTextField[k].setBounds(x +((i + 1)*60), y + ((j + 1)*35), 50, 30);        
+        if(i == 0 || j == 0)
+            jTextField[k].setBackground(new Color(51,255,153));
+        
+        add(jTextField[k]);
+    }
+}
